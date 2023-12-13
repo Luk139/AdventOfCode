4e53d5980c56ae9670e1c10e69c424ca80e96744
lufey <lukas.fey@smail.th-koeln.de>
2023-12-13 15:19:27 +0100

    commented day 1, still struggling with day 2

--- AOC1.py
+++ AOC1.py
@@ -1,55 +1,75 @@
-#from aoc_tools import *
-
+# Read the contents of the file "AOC1.txt"
 with open("AOC1.txt") as f:
     s = f.read()
 
-r = 0
-for x in s.strip().split("\n"):
-    fst = None
-    lst = None
-    for y in x:
-        if y.isdigit():
-            lst = y
-            if fst is None:
-                fst = y
-    r += int(fst + lst)
+# Initialize a variable to store the sum of first and last digits
+sum_first_last_digits = 0
 
-print(r)
+# Part 1: Calculate sum of first and last digits in each line
+for line in s.strip().split("\n"):
+    first_digit = None
+    last_digit = None
 
-m = {
-        "zero":0,
-        "one":1,
-        "two":2,
-        "three":3,
-        "four":4,
-        "five":5,
-        "six":6,
-        "seven":7,
-        "eight":8,
-        "nine":9,
-        }
+    # Iterate through each character in the line
+    for char in line:
+        # Check if the character is a digit
+        if char.isdigit():
+            last_digit = char
+            if first_digit is None:
+                first_digit = char
+
+    # If both first and last digits are found, add their sum to the total
+    if first_digit is not None and last_digit is not None:
+        sum_first_last_digits += int(first_digit + last_digit)
+
+# Print the sum of the first and last digits in each line
+print("Sum of first and last digits (Part 1):", sum_first_last_digits)
 
 
-r = 0
-for x in s.strip().split("\n"):
-    fst = None
-    lst = None
-    s = ""
-    for c in x:
-        dig = None
-        if c.isdigit():
-            dig = c
+# Define a dictionary mapping words to their numeric representations
+word_to_digit = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+# Initialize a variable to store the sum of interpreted digits
+sum_interpreted_digits = 0
+
+# Part 2: Calculate sum of interpreted digits (from words and/or digits) in each line
+for line in s.strip().split("\n"):
+    first_digit = None
+    last_digit = None
+    interpreted_string = ""
+
+    # Iterate through each character in the line
+    for char in line:
+        digit = None
+        if char.isdigit():
+            digit = char
         else:
-            s += c
-            for k,v in m.items():
-                if s.endswith(k):
-                    # process as digit v
-                    dig = str(v)
-        if dig is not None:
-            lst = dig
-            if fst is None:
-                fst = dig
+            interpreted_string += char
+            # Check if any word matches the end of the interpreted string
+            for word, value in word_to_digit.items():
+                if interpreted_string.endswith(word):
+                    # Process the word as its corresponding digit value
+                    digit = str(value)
 
-    r += int(fst + lst)
+        if digit is not None:
+            last_digit = digit
+            if first_digit is None:
+                first_digit = digit
 
-print(r)
+    # If both first and last digits are found, add their sum to the total
+    if first_digit is not None and last_digit is not None:
+        sum_interpreted_digits += int(first_digit + last_digit)
+
+# Print the sum of interpreted digits (from words and/or digits) in each line
+print("Sum of interpreted digits (Part 2):", sum_interpreted_digits)
--- AOC2.py
+++ AOC2.py
@@ -1,4 +1,36 @@
+import re
+
 with open("AOC2.txt") as f:
     s = f.read()
 
-print(s)
+input_red = 12
+input_blue = 14
+input_green = 13
+
+valid_games = []
+
+games = s.strip().split("\n")
+
+for game in games:
+    game_id, game_data = game.split(":")
+    sets = game_data.split(";")
+    cube = game_data.split(",")
+    cubes = {'red': 0, 'blue': 0, 'green': 0}
+    valid = True
+
+    for set_info in sets:
+        colors = re.findall(r'(\d+) (\w+)', set_info)
+
+        for count, color in colors:
+            cubes[color] += int(count)
+
+        if cubes['red'] > input_red or cubes['blue'] > input_blue or cubes['green'] > input_green:
+            valid = False
+            break
+
+    if valid and cubes['red'] <= input_red and cubes['blue'] <= input_blue and cubes['green'] <= input_green:
+        valid_games.append(game_id.strip())
+        print("Valid game:", game_id.strip(), "Cubes:", cubes)
+
+print("Possible games:", valid_games)
+print("Sum of IDs:", sum(map(int, [game.split()[1] for game in valid_games])))
